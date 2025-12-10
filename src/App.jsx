2a09242f71149ac5e30d665f5f4aa5504fbbb8bf
Rizal <84571944+theonlyrizal@users.noreply.github.com>
2025-12-11 04:55:34 +0600

    logo and home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,51 @@
+import { Outlet } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import AuthProvider from './context/AuthContext';
+
+// Layouts
+import Navbar from './components/common/Navbar';
+import Footer from './components/common/Footer';
+
 function App() {
-  return <div>root</div>;
+  return (
+    <AuthProvider>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        
+        <main className="flex-grow">
+          <Outlet />
+        </main>
+        
+        <Footer />
+      </div>
+      
+      {/* Toast Notifications */}
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+          success: {
+            duration: 3000,
+            iconTheme: {
+              primary: '#4ade80',
+              secondary: '#fff',
+            },
+          },
+          error: {
+            duration: 4000,
+            iconTheme: {
+              primary: '#ef4444',
+              secondary: '#fff',
+            },
+          },
+        }}
+      />
+    </AuthProvider>
+  );
 }
 
 export default App;
